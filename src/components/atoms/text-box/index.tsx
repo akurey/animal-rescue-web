f99d2ffb9409545ad4ed9c5cec3f602360def280
kris_duran <kduran@akurey.com>
2022-07-28 18:24:00 +0200

    Adding input validator

--- src/components/atoms/text-box/index.tsx
+++ src/components/atoms/text-box/index.tsx
@@ -6,7 +6,10 @@ interface TextBoxProps extends React.HTMLAttributes<HTMLInputElement> {
   disabled?: boolean;
   onChange?: (e: any) => void;
   onClick?: () => void;
-  value: string;
+  validators?: any[];
+  type?: string;
+  description?: string;
+  value?: string;
   placeholder: string;
 }
 
@@ -17,10 +20,24 @@ function TextBox({
   placeholder,
   value,
   onChange,
+  validators,
+  type,
+  description,
 }: TextBoxProps) {
   const [valueInput, setValueInput] = useState(value);
+  const [error, setError] = useState({ message: null, validator: () => {} });
+
+  const validateInput = (valueValidate: string) => {
+    const validationError = validators?.find(
+      (v) => !v.validator(valueValidate)
+    );
+    if (error !== validationError) {
+      setError(validationError);
+    }
+  };
 
   const onChangeInput = (e: any) => {
+    validateInput(e.target.value);
     setValueInput(e.target.value);
     if (onChange && typeof onChange === "function") {
       onChange(e);
@@ -28,15 +45,19 @@ function TextBox({
   };
 
   return (
-    <input
-      type="textbox"
-      onClick={onClick}
-      className={`input ${textBoxStyle}`}
-      disabled={disabled}
-      placeholder={placeholder}
-      onChange={onChangeInput}
-      value={valueInput}
-    />
+    <div>
+      <p>{description}</p>
+      <input
+        type={type}
+        onClick={onClick}
+        className={`input ${textBoxStyle} ${error?.message && "input--error"}`}
+        disabled={disabled}
+        placeholder={placeholder}
+        onChange={onChangeInput}
+        value={valueInput}
+      />
+      {error?.message && <p className="message--error">{error?.message}</p>}
+    </div>
   );
 }
 
--- src/components/pages/home/index.tsx
+++ src/components/pages/home/index.tsx
@@ -1,8 +1,24 @@
 import React from "react";
+import validator from "validator";
+import TextBox from "../../atoms/text-box";
 import "./home.scss";
 
 function Home() {
-  return <p>Home</p>;
+  return (
+    <TextBox
+      placeholder="test"
+      validators={[
+        {
+          validator: (val: string) => !validator.isEmpty(val),
+          message: "Vacio",
+        },
+        {
+          validator: (value: string) => validator.isLength(value, { min: 3 }),
+          message: "chiquito",
+        },
+      ]}
+    />
+  );
 }
 
 export default Home;
