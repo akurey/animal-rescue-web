564ce137549c5a7ad78a4766115bd41df43260ef
kris_duran <kduran@akurey.com>
2022-07-28 18:24:14 +0200

    Adding input validator

--- src/components/pages/home/index.tsx
+++ src/components/pages/home/index.tsx
@@ -1,24 +1,8 @@
 import React from "react";
-import validator from "validator";
-import TextBox from "../../atoms/text-box";
 import "./home.scss";
 
 function Home() {
-  return (
-    <TextBox
-      placeholder="test"
-      validators={[
-        {
-          validator: (val: string) => !validator.isEmpty(val),
-          message: "Vacio",
-        },
-        {
-          validator: (value: string) => validator.isLength(value, { min: 3 }),
-          message: "chiquito",
-        },
-      ]}
-    />
-  );
+  return <p>Home</p>;
 }
 
 export default Home;
