03eb90a441b04e26905222476591b6a9fa7ed696
Kristal Durán <49170493+kduranaraya@users.noreply.github.com>
2022-07-29 09:14:48 -0600

    Feat/arw-4 Text Area (#32)
    * Adding text-area component
    
    * Adding text-area component
    
    * Adding label

---
+++ src/components/atoms/text-area/index.tsx
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import "./styles.scss";
+
+interface TextAreaProps extends React.HTMLAttributes<HTMLInputElement> {
+  textAreaStyle?: string;
+  disabled?: boolean;
+  onChange?: (e: any) => void;
+  onClick?: () => void;
+  description?: string;
+  value: string;
+  placeholder: string;
+}
+
+function TextArea({
+  onClick,
+  textAreaStyle,
+  disabled,
+  placeholder,
+  description,
+  value,
+  onChange,
+}: TextAreaProps) {
+  const [valueInput, setValueInput] = useState(value);
+
+  const onChangeInput = (e: any) => {
+    setValueInput(e.target.value);
+    if (onChange && typeof onChange === "function") {
+      onChange(e);
+    }
+  };
+
+  return (
+    <div className="textarea">
+      <label htmlFor="textarea">{description}</label>
+      <textarea
+        id="textarea"
+        onClick={onClick}
+        className={`input ${textAreaStyle}`}
+        disabled={disabled}
+        placeholder={placeholder}
+        onChange={onChangeInput}
+        value={valueInput}
+      />
+    </div>
+  );
+}
+
+export default TextArea;
