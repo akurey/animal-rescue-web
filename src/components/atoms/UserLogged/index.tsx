7ff3cee23b77eda1a4de98fc72bc6b6755902e0a
pdinarte <99428207+pdinarte@users.noreply.github.com>
2022-08-09 16:01:53 -0600

    UserLogged Component

---
+++ src/components/atoms/UserLogged/index.tsx
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import "./styles.scss";
+
+interface UserLoggedProps {
+  name: string;
+  lastname: string;
+  photo?: string;
+}
+
+function UserLogged ({
+  name,
+  lastname,
+  photo,
+}: UserLoggedProps) {
+
+  const goToProfile = () => {
+
+  }
+
+  return (
+    <>
+    <button className="user-profile" onClick={goToProfile}>
+      <div>
+        <h1>Mi perfil</h1>
+        <h2>{name + " " + lastname}</h2>
+      </div>
+      {
+        photo ? <img src={photo} alt="" /> :
+        <div className="profile-img">
+          <div className="initials">{name[0]+lastname[0]}</div>
+        </div>
+      }
+    </button>
+    </>
+  )
+}
+
+export default UserLogged;
