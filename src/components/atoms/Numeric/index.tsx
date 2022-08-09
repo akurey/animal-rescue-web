367c7cb71c4626f1f4987709e5de447b54b5c05a
pdinarte <99428207+pdinarte@users.noreply.github.com>
2022-08-09 03:55:02 -0600

    Numeric Component (#40)

---
+++ src/components/atoms/Numeric/index.tsx
@@ -0,0 +1,80 @@
+import React, { useState } from "react";
+import "./styles.scss";
+
+interface NumericProps extends React.HTMLAttributes<HTMLInputElement> {
+  numericStyle?: string;
+  disabled?: boolean;
+  onChange?: (e: any) => void;
+  onClick?: () => void;
+  validators?: any[];
+  description?: string;
+  value?: number;
+  className?: string;
+  placeholder?: string;
+  max?: number;
+  min?: number;
+  step?: number;
+}
+
+function Numeric({
+  onClick,
+  numericStyle,
+  disabled,
+  placeholder,
+  value,
+  onChange,
+  validators,
+  description,
+  className,
+  max,
+  min,
+  step
+}: NumericProps) {
+  const [valueInput, setValueInput] = useState(value);
+  const [error, setError] = useState({ message: null, validator: () => {} });
+
+  const validateInput = (valueValidate: string) => {
+    const validationError = validators?.find(
+      (v) => !v.validator(valueValidate)
+    );
+    if (error !== validationError) {
+      setError(validationError);
+    }
+  };
+
+  const onChangeInput = (e: any) => {
+    validateInput(e.target.value);
+    setValueInput(e.target.value);
+    if (onChange && typeof onChange === "function") {
+      onChange(e);
+    }
+  };
+  return (
+    <>
+      <label htmlFor="input" className="input--label">
+        {description}
+      </label>
+      <input 
+      id="numeric"
+      type="number"
+      onClick={onClick}
+      className={`
+      input
+      ${numericStyle}
+      ${error?.message && "input--error"}
+      ${className}
+      `}
+      disabled={disabled}
+      placeholder={placeholder}
+      onChange={onChangeInput}
+      value={valueInput}
+      max={max}
+      min={min}
+      step={step}
+      />
+      {error?.message && <p className="message--error">{error?.message}</p>}
+    </>
+  );
+}
+
+export default Numeric;
--- src/components/atoms/index.tsx
+++ src/components/atoms/index.tsx
@@ -1,10 +1,12 @@
 import ButtonComponent from "./Button";
 import DropdownComponent from "./Dropdown";
+import NumericComponent from "./Numeric";
 import TextBoxComponent from "./TextBox";
 import TextAreaComponent from "./TextArea";
 
 export const Button = ButtonComponent;
 export const Dropdown = DropdownComponent;
+export const Numeric = NumericComponent;
 export const TextBox = TextBoxComponent;
 export const TextArea = TextAreaComponent;
 
