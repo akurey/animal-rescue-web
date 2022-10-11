e33f74c98894b9bb23afbaa25f94e72ffb125e7b
pdinarte <99428207+pdinarte@users.noreply.github.com>
2022-10-11 16:12:22 -0600

    Animal list with background

--- src/components/atoms/ImageBackground/index.tsx
+++ src/components/atoms/ImageBackground/index.tsx
@@ -1,14 +1,20 @@
 import React from "react";
 import "./styles.scss";
 import OceloteImg from "../../../assets/backgrounds/Ocelote.png";
+import TucanImg from "../../../assets/backgrounds/Tucan.png";
 
-function ImageBackground() {
+interface ImageBackgroundProps extends React.HTMLAttributes<HTMLImageElement> {
+  imageName: String;
+  alt?: String;
+}
+
+function ImageBackground({ imageName, alt }: ImageBackgroundProps) {
   return (
     <div className="background-container">
       <img
         className="background-image"
-        src={OceloteImg}
-        alt=""
+        src={imageName === "Ocelote.png" ? OceloteImg : TucanImg}
+        alt={alt && ""}
         role="presentation"
       />
       <div className="background-blur" />
--- src/components/pages/rescues/index.tsx
+++ src/components/pages/rescues/index.tsx
@@ -14,94 +14,92 @@ function RescuesComponente() {
   return (
     <>
       {/* TODO add background component */}
-      <div className="page-layout">
-        {/* TODO add nav bar */}
-        <Breadcrumbs />
-        <h1>Animales Rescatados</h1>
-        <Button onClick={() => {}} className="rescues--button">
-          <AddIcon fontSize="medium" /> nuevo animal
-        </Button>
+      {/* TODO add nav bar */}
+      <Breadcrumbs />
+      <h1>Animales Rescatados</h1>
+      <Button onClick={() => {}} className="rescues--button">
+        <AddIcon fontSize="medium" /> nuevo animal
+      </Button>
 
-        <div className="rescues--filters">
-          <Button
-            onClick={() => setFilter(REVIEW)}
-            className={`rescues--filter ${
-              filter === REVIEW && "rescues--filter-active"
-            }`}
-          >
-            En Revisión
-          </Button>
-          <Button
-            onClick={() => setFilter(PUBLIC)}
-            className={`rescues--filter ${
-              filter === PUBLIC && "rescues--filter-active"
-            }`}
-          >
-            Públicos
-          </Button>
-        </div>
-        <Table
-          // TODO: update real data
-          items={[
-            {
-              id: "1",
-              name: "Perro",
-              comunName: "Firulais",
-              date: "29/07/2022",
-              place: "Málaga",
-              distribution: "-",
-            },
-            {
-              id: "2",
-              name: "Perro",
-              comunName: "Firulais",
-              date: "29/07/2022",
-              place: "Málaga",
-              distribution: "-",
-            },
-            // {
-            //   id: "3",
-            //   name: "Perro",
-            //   comunName: "Firulais",
-            //   date: "29/07/2022",
-            //   place: "Málaga",
-            //   distribution: "-",
-            // },
-            // {
-            //   id: "4",
-            //   name: "Perro",
-            //   comunName: "Firulais",
-            //   date: "29/07/2022",
-            //   place: "Málaga",
-            //   distribution: "-",
-            // },
-            // {
-            //   id: "5",
-            //   name: "Perro",
-            //   comunName: "Firulais",
-            //   date: "29/07/2022",
-            //   place: "Málaga",
-            //   distribution: "-",
-            // },
-            // {
-            //   id: "6",
-            //   name: "Perro",
-            //   comunName: "Firulais",
-            //   date: "29/07/2022",
-            //   place: "Málaga",
-            //   distribution: "-",
-            // },
-            // {
-            //   id: "7",
-            //   name: "Perro",
-            //   comunName: "Firulais",
-            //   date: "29/07/2022",
-            //   place: "Málaga",
-            //   distribution: "-",
-            // },
-          ]}
-        />
+      <div className="rescues--filters">
+        <Button
+          onClick={() => setFilter(REVIEW)}
+          className={`rescues--filter ${
+            filter === REVIEW && "rescues--filter-active"
+          }`}
+        >
+          En Revisión
+        </Button>
+        <Button
+          onClick={() => setFilter(PUBLIC)}
+          className={`rescues--filter ${
+            filter === PUBLIC && "rescues--filter-active"
+          }`}
+        >
+          Públicos
+        </Button>
       </div>
+      <Table
+        // TODO: update real data
+        items={[
+          {
+            id: "1",
+            name: "Perro",
+            comunName: "Firulais",
+            date: "29/07/2022",
+            place: "Málaga",
+            distribution: "-",
+          },
+          {
+            id: "2",
+            name: "Perro",
+            comunName: "Firulais",
+            date: "29/07/2022",
+            place: "Málaga",
+            distribution: "-",
+          },
+          // {
+          //   id: "3",
+          //   name: "Perro",
+          //   comunName: "Firulais",
+          //   date: "29/07/2022",
+          //   place: "Málaga",
+          //   distribution: "-",
+          // },
+          // {
+          //   id: "4",
+          //   name: "Perro",
+          //   comunName: "Firulais",
+          //   date: "29/07/2022",
+          //   place: "Málaga",
+          //   distribution: "-",
+          // },
+          // {
+          //   id: "5",
+          //   name: "Perro",
+          //   comunName: "Firulais",
+          //   date: "29/07/2022",
+          //   place: "Málaga",
+          //   distribution: "-",
+          // },
+          // {
+          //   id: "6",
+          //   name: "Perro",
+          //   comunName: "Firulais",
+          //   date: "29/07/2022",
+          //   place: "Málaga",
+          //   distribution: "-",
+          // },
+          // {
+          //   id: "7",
+          //   name: "Perro",
+          //   comunName: "Firulais",
+          //   date: "29/07/2022",
+          //   place: "Málaga",
+          //   distribution: "-",
+          // },
+        ]}
+      />
     </>
   );
 }
