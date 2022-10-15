52a13788ffde7355ed4e3dd88ecf18cc591100f6
luisaguilar <laguilar@akurey.com>
2022-10-15 16:10:02 -0600

    [feat][ARW-51] Add break points and 404 page

--- src/components/molecules/NavigationBar/index.tsx
+++ src/components/molecules/NavigationBar/index.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import UserLogged from "../../atoms/UserLogged";
 import "./styles.scss";
+import homeIcon from "../../../assets/images/logo.png";
 
 function NavigationBar() {
   return (
     <header className="navigation-container">
       <nav className="navigation-bar">
         {/* TODO change this symbol for the real house Icon */}
-        <div className="navigation-bar__home-icon">&#8962;</div>
+        <img className="navigation-bar__home-icon" src={homeIcon} alt="" />
         <UserLogged />
       </nav>
     </header>
---
+++ src/components/pages/404/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./404.scss";
+
+function NotFound() {
+  return <p>404</p>;
+}
+
+export default NotFound;
