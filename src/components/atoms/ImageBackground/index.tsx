e3515dbdabe0cbf9e33c33fc174d7380e9ef4d11
pdinarte <99428207+pdinarte@users.noreply.github.com>
2022-10-24 18:16:38 -0600

    Dynamic background image

--- src/components/atoms/ImageBackground/index.tsx
+++ src/components/atoms/ImageBackground/index.tsx
@@ -8,36 +8,44 @@ import IguanaImg from "../../../assets/backgrounds/Iguana.png";
 
 function ImageBackground() {
   const [backgroundImage, setBackgroundImage] = useState(OceloteImg);
+  const [imagePosition, setImagePosition] = useState("top-right");
   const location = useLocation();
 
   useEffect(() => {
     switch (location.pathname) {
       case "/login":
         setBackgroundImage(RanaImg);
+        setImagePosition("top-right");
         break;
       case "/rescues":
         setBackgroundImage(OceloteImg);
+        setImagePosition("top-right");
         break;
       case "/rescues/public":
         setBackgroundImage(TucanImg);
+        setImagePosition("top-right");
         break;
       case "/rescues/new":
         setBackgroundImage(IguanaImg);
+        setImagePosition("bottom-left");
         break;
       default:
         setBackgroundImage(OceloteImg);
+        setImagePosition("top-right");
     }
   }, [location]);
 
   return (
-    <div className="background-container">
+    <div className={`background-container ${imagePosition}`}>
       <img
         className="background-image"
         src={backgroundImage}
         alt=""
         role="presentation"
       />
-      <div className="background-blur" />
+      {location.pathname !== "/rescues/new" && (
+        <div className="background-blur" />
+      )}
     </div>
   );
 }
