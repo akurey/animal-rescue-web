32d10883778ba49f141c5ca303c7befe27868fe8
ejimenezak <ejimenez@akurey.com>
2023-02-06 14:01:53 -0600

    [ARW-NewAnimalForm][fix] Gitignore and other minor changes

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -6,12 +6,12 @@ const baseURL = process.env.REACT_APP_BASE_URL;
 // let headers = {};
 // if (localStorage.token) {
 //   headers = {
-//     // Authorization: `Bearer ${localStorage.getItem("access_token")}`,
-//     "Access-Control-Allow-Origin": "*",
-//     "Access-Control-Allow-Methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
-//     // "Access-Control-Allow-Credentials": "false",
-//     // "Access-Control-Allow-Headers":
-//     // "Origin, X-Requested-With, Content-Type, Accept",
+//     //   Authorization: `Bearer ${localStorage.getItem("access_token")}`,
+//     //   "Access-Control-Allow-Origin": "*",
+//     //   "Access-Control-Allow-Methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+//     //   "Access-Control-Allow-Credentials": "false",
+//     //   "Access-Control-Allow-Headers":
+//     //     "Origin, X-Requested-With, Content-Type, Accept",
 //   };
 // }
 
