b280ca563994c3bc050c98aa7c5b5abbbdabf89f
Esteban Jimenez <88204114+ejimenezak@users.noreply.github.com>
2023-02-21 08:21:54 -0600

    [ARW-70][feat] Autofill input with fetched animal data (#74)
    * [ARW-NewAnimalForm] Refactor new animal form basic functionality
    
    * [ARW-NewAnimalForm][fix] Gitignore and other minor changes
    
    * [ARW-NewAnimalForm] Delete comments
    
    * [ARW-NewAnimalForm] Hide SHOW & EDIT buttons from the rescues table
    
    * [ARW-NewAnimalForm] Small Fix
    
    * [ARW-NewAnimalForm] Small Fix
    
    * [ARW-70][feat] Autofill input with fetched animal data
    
    * [ARW-70] Fix comment
    
    * [ARW-70][Fix] Avoid key errors

--- src/components/molecules/AnimalForm/index.tsx
+++ src/components/molecules/AnimalForm/index.tsx
@@ -1,16 +1,25 @@
 import React from "react";
+import { Autocomplete, TextField } from "@mui/material";
 import RenderType from "./helper/elementRenderer";
 import { RescueObservable } from "../../../observables/rescue.observable";
 import { useObservable } from "../../../hooks/use-observable.hook";
 import "./styles.scss";
 
 interface FormProps {
+  setAnimalIdToSend?: React.Dispatch<React.SetStateAction<Number>>;
+  animalData?: any[];
   fields: any[];
   types: any[];
   section: string;
 }
 
-export default function FormPage({ fields, types, section }: FormProps) {
+export default function FormPage({
+  setAnimalIdToSend,
+  animalData,
+  fields,
+  types,
+  section,
+}: FormProps) {
   const [rescue] = useObservable(RescueObservable.rescue$);
 
   const handleChange = (event) => {
@@ -25,8 +34,43 @@ export default function FormPage({ fields, types, section }: FormProps) {
     RescueObservable.setRescue(update);
   };
 
+  const defaultProps = {
+    options: animalData,
+    getOptionLabel: (option: AnimalDataType) =>
+      `${option.Name} (${option.ScientificName})`,
+  };
+
+  interface AnimalDataType {
+    Name: string;
+    ScientificName: string;
+    id: Number;
+  }
+
   return (
     <div className="form-column">
+      {animalData && (
+        <Autocomplete
+          // eslint-disable-next-line react/jsx-props-no-spreading
+          {...defaultProps}
+          autoHighlight
+          freeSolo
+          disableClearable
+          onChange={(event: any, newValue: AnimalDataType | null) => {
+            setAnimalIdToSend(newValue.id);
+          }}
+          fullWidth
+          includeInputInList
+          renderInput={(params) => (
+            <TextField
+              // eslint-disable-next-line react/jsx-props-no-spreading
+              {...params}
+              className="autoComplete"
+              label="Nombre Común (Especie)"
+              variant="standard"
+            />
+          )}
+        />
+      )}
       {types.map((data, index) => (
         // eslint-disable-next-line react/no-array-index-key
         <div key={index} className="form-row">
